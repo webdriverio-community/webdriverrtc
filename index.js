a535714074d5a38585e2159c150b68ff71de6052
christian-bromann <contact@christian-bromann.com>
2015-01-17 18:52:00 +0100

    implemented basic WebdriverRTC functionality with two commands: startAnalyzing and getLatestStats

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * WebdriverRTC
+ * WebRTC testing tool for WebdriverIO
+ *
+ * @author Christian Bromann <christian@saucelabs.com>
+ * @author Thomas Fett <mail@thomasfett.de>
+ *
+ * @license Licensed under the MIT license.
+ */
+
+module.exports = require('./lib/webdriverrtc');
