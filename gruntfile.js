41f6b884b976b9fc804e0b0fe7610cbac54ccc37
christian-bromann <contact@christian-bromann.com>
2016-08-11 17:49:41 +0200

    rewritten project into ES2016

---
+++ gruntfile.js
@@ -0,0 +1,60 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkgFile: 'package.json',
+        clean: ['build'],
+        babel: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: './lib',
+                    src: ['**/*.js'],
+                    dest: 'build',
+                    ext: '.js'
+                }]
+            }
+        },
+        eslint: {
+            options: {
+                parser: 'babel-eslint'
+            },
+            target: ['index.js', 'lib/**/*.js', 'examples/**/*.js'] //, 'test/**/*.js']
+        },
+        contributors: {
+            options: {
+                commitMessage: 'update contributors'
+            }
+        },
+        bump: {
+            options: {
+                commitMessage: 'v%VERSION%',
+                pushTo: 'upstream'
+            }
+        },
+        watch: {
+            commands: {
+                files: ['index.js', 'lib/**/*.js'],
+                tasks: ['babel:commands'],
+                options: {
+                    spawn: false
+                }
+            }
+        }
+    })
+
+    require('load-grunt-tasks')(grunt)
+    grunt.registerTask('default', ['build'])
+    grunt.registerTask('build', 'Build webdriverrtc', function () {
+        grunt.task.run([
+            'eslint',
+            'clean',
+            'babel'
+        ])
+    })
+    grunt.registerTask('release', 'Bump and tag version', function (type) {
+        grunt.task.run([
+            'build',
+            'contributors',
+            'bump:' + (type || 'patch')
+        ])
+    })
+}
