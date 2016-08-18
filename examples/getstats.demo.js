66732ade105f1999cd087a2f0486db381defea58
christian-bromann <contact@christian-bromann.com>
2016-08-18 16:56:17 +0200

    example eslint fix

--- examples/getstats.demo.js
+++ examples/getstats.demo.js
@@ -1,3 +1,5 @@
+/* global appController */
+
 /**
  * Example script that opens "https://apprtc.appspot.com" creates an WebRTC connection
  * and traces this connection for 10 seconds to print the available results
@@ -6,24 +8,24 @@
  * @license MIT
  */
 
-var WebdriverIO = require('webdriverio'),
-    WebdriverRTC = require('../'),
-    path = require('path'),
-    fs = require('fs'),
-    args = [
-        "use-fake-device-for-media-stream",
-        "use-fake-ui-for-media-stream"
-    ];
+var WebdriverIO = require('webdriverio')
+var WebdriverRTC = require('../')
+var path = require('path')
+var fs = require('fs')
+var args = [
+    'use-fake-device-for-media-stream',
+    'use-fake-ui-for-media-stream'
+]
 
 /**
  * check if fake videos exist
  */
-var argsBrowserA = args.slice(0, args.length);
-if(fs.existsSync(path.join(__dirname, '..', 'sign_irene_qcif.y4m'))) {
+var argsBrowserA = args.slice(0, args.length)
+if (fs.existsSync(path.join(__dirname, '..', 'sign_irene_qcif.y4m'))) {
     argsBrowserA.push('use-file-for-fake-video-capture=' + path.join(__dirname, '..', 'sign_irene_qcif.y4m'))
 }
-var argsBrowserB = args.slice(0, args.length);
-if(fs.existsSync(path.join(__dirname, '..', 'silent_qcif.y4m'))) {
+var argsBrowserB = args.slice(0, args.length)
+if (fs.existsSync(path.join(__dirname, '..', 'silent_qcif.y4m'))) {
     argsBrowserB.push('use-file-for-fake-video-capture=' + path.join(__dirname, '..', 'silent_qcif.y4m'))
 }
 
@@ -40,11 +42,11 @@ var matrix = WebdriverIO.multiremote({
             chromeOptions: { args: argsBrowserB }
         }
     }
-});
+})
 
-WebdriverRTC.init(matrix);
+WebdriverRTC.init(matrix)
 
-var channel = Math.round(Math.random() * 100000000000);
+var channel = Math.round(Math.random() * 100000000000)
 
 matrix
     .init()
@@ -52,18 +54,18 @@ matrix
     .click('#confirm-join-button')
     .pause(5000)
     .startAnalyzing({
-        selectorMethod: function() {
-            return appController.call_.pcClient_.pc_;
+        selectorMethod: function () {
+            return appController.call_.pcClient_.pc_
         }
     })
-    .getConnectionInformation(function(err, connectionType) {
-        console.log(connectionType);
+    .getConnectionInformation().then(function (connectionType) {
+        console.log(connectionType)
     })
     .pause(10000)
-    .getStats(10000, function(err, mean, median, max, min) {
-        console.log('mean:', mean);
-        console.log('median:', median);
-        console.log('max:', max);
-        console.log('min:', min);
+    .getStats(10000).then(function (mean, median, max, min) {
+        console.log('mean:', mean)
+        console.log('median:', median)
+        console.log('max:', max)
+        console.log('min:', min)
     })
-    .end();
+    .end()
--- examples/scream.demo.js
+++ examples/scream.demo.js
@@ -1,3 +1,5 @@
+/* global appController */
+
 /**
  * This tests fails if you clap or scream while the test is running
  *
@@ -5,10 +7,10 @@
  * @license MIT
  */
 
-var WebdriverIO = require('webdriverio'),
-    WebdriverRTC = require('../'),
-    assert = require('assert'),
-    inputLevel, res;
+var WebdriverIO = require('webdriverio')
+var WebdriverRTC = require('../')
+var assert = require('assert')
+var inputLevel
 
 var matrix = WebdriverIO.multiremote({
     browserA: {
@@ -23,11 +25,11 @@ var matrix = WebdriverIO.multiremote({
             chromeOptions: { args: ['use-fake-ui-for-media-stream'] }
         }
     }
-});
+})
 
-WebdriverRTC.init(matrix);
+WebdriverRTC.init(matrix)
 
-var channel = Math.round(Math.random() * 100000000000);
+var channel = Math.round(Math.random() * 100000000000)
 
 matrix
     .init()
@@ -35,14 +37,14 @@ matrix
     .click('#confirm-join-button')
     .pause(5000)
     .startAnalyzing({
-        selectorMethod: function() {
-            return appController.call_.pcClient_.pc_;
+        selectorMethod: function () {
+            return appController.call_.pcClient_.pc_
         }
     })
     .pause(5000)
-    .getStats(5000, function(err, mean, median, max, min, results) {
-        inputLevel = max.audio.outbound.inputLevel;
+    .getStats(5000).then(function (mean, median, max, min, results) {
+        inputLevel = max.audio.outbound.inputLevel
     })
-    .end(function() {
-        assert.ok(inputLevel < 5000, 'This was too loud! Your audio input level was ' + inputLevel + '.');
-    });
+    .end().then(function () {
+        assert.ok(inputLevel < 5000, 'This was too loud! Your audio input level was ' + inputLevel + '.')
+    })
